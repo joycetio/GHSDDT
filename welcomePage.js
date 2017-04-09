9a649310d1bbac3507e9034421228d86ed4aafb1
Marie Joyce Tio <joycetio@joyces-mbp.gusd.lcl>
2017-04-08 17:03:49 -0700

    add images, welcome.js fuile, & update styles.css & html files

---
+++ welcomePage.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    //fullscreen slider
+    $('.slider').slider();
+
+    //on click function for home page in welcome.html
+    $(".homeBtn").on("click", function() {
+        window.location.href = "index.html"
+    });
+
+    //on click function for events page in welcome.html
+
+
+    //on click function for videos page in welcome.html
+    
+
+    //on click function for contact page in welcome.html 
+    $(".contactBtn").on("click", function() {
+        window.location.href = "contact.html"
+    });
+});
